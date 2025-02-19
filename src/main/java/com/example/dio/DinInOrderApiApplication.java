50cb840bd4079903f5680fe81975ca2f9ef297e5
ranjitha1595 <ranjithasr32@gmail.com>
2025-02-19 16:18:07 +0530

    project initialized

---
+++ src/main/java/com/example/dio/DinInOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinInOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinInOrderApiApplication.class, args);
+	}
+
+}
