9718346e082c204f5e24652d9b5d53a94078c2f7
ranjitha1595 <ranjithasr32@gmail.com>
2025-02-27 17:19:31 +0530

    adding two methods to reduce the code redundancy while registering a Role

--- src/main/java/com/example/dio/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/dio/service/impl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.dio.service.impl;
 
+import com.example.dio.enums.UserRole;
+import com.example.dio.model.Admin;
+import com.example.dio.model.Staff;
 import com.example.dio.model.User;
 import com.example.dio.repository.UserRepository;
 import com.example.dio.service.UserService;
@@ -14,7 +17,24 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User registerUser(User user) {
+        User user2 = this.createUserByRole(user.getRole());
 
+        this.mapToNewUser(user,user2);
         return userRepository.save(user);
+    }
+
+    private void mapToNewUser(User user, User user2) {
+        user2.setUsername(user.getUsername());
+        user2.setEmail(user.getEmail());
+    }
+
+    private User createUserByRole(UserRole role){
+        User user;
+        switch(role){
+            case ADMIN -> user = new Admin();
+            case STAFF -> user = new Staff();
+            default -> throw new RuntimeException("failed to register user, invalid user");
+        }
+        return user;
     }
 }
