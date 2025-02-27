020ba37d8551337ad6515aff39b539501406be79
ranjitha1595 <ranjithasr32@gmail.com>
2025-02-27 16:16:02 +0530

    creating the ResponseBuilder Class

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.dio.controller;
 
 import com.example.dio.model.User;
 import com.example.dio.service.impl.UserServiceImpl;
+import com.example.dio.util.ResponseBuilder;
 import com.example.dio.util.ResponseStructure;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,14 +19,32 @@ public class UserController {
     private final UserServiceImpl userService;
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseStructure<User>> registerUser(@RequestBody User user){
-        user=userService.registerUser(user);
+    public ResponseEntity<ResponseStructure<User>> registerUser(@RequestBody User user) {
+        user = userService.registerUser(user);
 
-        ResponseStructure<User> structure = new ResponseStructure<>();
-        structure.setData(user);
-        structure.setStatus(HttpStatus.CREATED.value());
-        structure.setMessage("User Created");
-
-        return new ResponseEntity<ResponseStructure<User>>(structure, HttpStatus.CREATED);
+        return ResponseBuilder.success(HttpStatus.CREATED, "User Created", user);
     }
+
+
+
+//    @PostMapping("/register")
+//    public ResponseEntity<ResponseStructure<User>> registerUser(@RequestBody User user){
+//        user=userService.registerUser(user);
+//
+//        ResponseStructure<User> structure = new ResponseStructure<>();
+//        structure.setData(user);
+//        structure.setStatus(HttpStatus.CREATED.value());
+//        structure.setMessage("User Created");
+//
+//        return new ResponseEntity<ResponseStructure<User>>(structure, HttpStatus.CREATED);
+//    }
+//
+//    private static <T> ResponseEntity<ResponseStructure<T>> success(HttpStatus status, String message, T data){
+//        ResponseStructure<User> structure = ResponseStructure.<User>builder()
+//                .status(status.value())
+//                .message(message)
+//                .data((User) data)
+//                .build();
+//        return ResponseEntity.status(HttpStatus.CREATED).body(structure);
+//    }
 }
--- src/main/java/com/example/dio/util/ResponseStructure.java
+++ src/main/java/com/example/dio/util/ResponseStructure.java
@@ -1,14 +1,23 @@
 package com.example.dio.util;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @Getter
-@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseStructure<T> {
     private int status;
     private String message;
     private T data;
 
+   public static <T> ResponseStructure<T> factory(HttpStatus status, String message, T data){
+       ResponseStructure<T> response = new ResponseStructure<>();
+       response.status=status.value();
+       response.message=message;
+       response.data=data;
 
+       return response;
+   }
 }
