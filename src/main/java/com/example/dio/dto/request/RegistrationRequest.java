c4d853c2952cbaae1dd4c5bc3ee8ef995cb47d9d
ranjitha1595 <ranjithasr32@gmail.com>
2025-02-28 16:37:57 +0530

    creating registrationrequest class

--- src/main/java/com/example/dio/dto/request/RegistrationRequest.java
+++ src/main/java/com/example/dio/dto/request/RegistrationRequest.java
@@ -1,4 +1,18 @@
 package com.example.dio.dto.request;
 
+import com.example.dio.enums.UserRole;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
 public class RegistrationRequest {
+
+    private String username;
+    private String email;
+    private String password;
+    private String phno;
+    private UserRole role;
 }
