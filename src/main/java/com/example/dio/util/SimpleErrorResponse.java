d8780c5b853a0d7c284dcdd73c9d13fc4bfe439c
ranjitha1595 <ranjithasr32@gmail.com>
2025-02-28 13:10:27 +0530

    creating simpleerrorresponse class

--- src/main/java/com/example/dio/util/SimpleErrorResponse.java
+++ src/main/java/com/example/dio/util/SimpleErrorResponse.java
@@ -1,4 +1,14 @@
 package com.example.dio.util;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class SimpleErrorResponse {
+
+    private String type;
+    private int status; // 404
+    private String message; // failed to update the user, The user is not found by the given id
+
 }
